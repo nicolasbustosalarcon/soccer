ed6e2d23f94ccebaa9580ea5b7a07780416ddcf2
Luis Fuenzalida Lizana <djk.tre@gmail.com>
2019-05-29 19:17:36 -0400

    letra

--- resources/views/jugador/edit.blade.php
+++ resources/views/jugador/edit.blade.php
@@ -22,7 +22,7 @@
 				</div>
 			</div>
 			<div class="form-group">
-			<label for="">Nacimiento</label>
+			<label for=""></label>
 			<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
 
 				<div class="form-group">
