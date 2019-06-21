2e9f964b0f66d8d7d3462fc15a31eae7de55eb58
nicolasbustosalarcon <nicolasbustos1996@gmail.com>
2019-06-21 01:57:05 -0400

    PartidoController

--- app/Http/Controllers/PartidoController.php
+++ app/Http/Controllers/PartidoController.php
@@ -1,12 +1,8 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Input;
-
-
 use App\Partido;
 use App\Club;
 use App\Asociacion;
@@ -19,14 +15,9 @@ use App\Jugador;
 use App\TrayectoriaJugador;
 use App\Historial;
 use DB;
-
-
-
-
 class PartidoController extends Controller
 {
 //--------Función que retorna lo que se mostrará en el index--------------------------------------------------------
-
     /**
      * Display a listing of the resource.
      *
@@ -45,45 +36,31 @@ class PartidoController extends Controller
         $year = $hoy['year'];
         $fecha = "$year"."-"."$mes"."-"."$dia";
         
-
-
-
         return view('partido.index',  ['partidos' => $partidos, 'clubes' => $clubes, 'torneos' => $torneos, 'fecha' =>$fecha]);
-
     }
-
     public function search($search){
-
         $search = urldecode($search);
         $club = Club::select()
                 ->where('nombreClub', 'LIKE', '%'.$search.'%')
                 ->orderBy('idClub', 'desc')
                 ->get();
-
         $jugadores = Jugador::select()
                 ->where('nombreJugador', 'LIKE', '%'.$search.'%')
                 ->orwhere('apellidosJugador', 'LIKE', '%'.$search.'%')
                 ->orderBy('idJugador', 'desc')
                 ->get();
-
         $torneos = Torneo::select()
                 ->where('nombreTorneo', 'LIKE', '%'.$search.'%')
                 ->orderBy('idTorneo', 'desc')
                 ->get();
-
         $estadios = Estadio::select()
                 ->where('nombreEstadio', 'LIKE', '%'.$search.'%')
                 ->orderBy('idEstadio', 'desc')
                 ->get();
-
-
-
         return view('partido.search',  ['club' => $club, "jugadores" => $jugadores,  "torneos" => $torneos,  "estadios" => $estadios]);
         
     }
 //-------------------------------------------------------------------------------------------------------------------
-
-
 //------Función que retorna las variables para la vista create-------------------------------------------------------
     /**
      * Show the form for creating a new resource.
@@ -93,7 +70,6 @@ class PartidoController extends Controller
     public function create(Request $request)
     {   
         $request->user()->authorizeRoles('admin'); //Se valida que el usuario que verá estos datos sea de tipo administrador
-
         $asociaciones=Asociacion::all();
         $clubes=Club::all();
         $ciudades=Ciudad::all();
@@ -101,12 +77,9 @@ class PartidoController extends Controller
         $paises=Pais::all();
         $torneos=Torneo::all();
         $arbitros=Arbitro::all();
-
         return view('partido.create', ['asociaciones' => $asociaciones->toArray(), 'ciudades' => $ciudades->toArray(), 'clubes' => $clubes->toArray(), 'estadios' => $estadios->toArray(), 'paises' => $paises->toArray(), 'torneos' => $torneos->toArray(), 'arbitros' => $arbitros->toArray()]);
     }
 //------------------------------------------------------------------------------------------------------------------
-
-
 //----------------Función para guardar un nuevo dato-----------------------------------------------------------------
     /**
      * Store a newly created resource in storage.
@@ -117,7 +90,6 @@ class PartidoController extends Controller
     public function store(Request $request)
     {
         $request->user()->authorizeRoles('admin'); //Se valida que el usuario que verá estos datos sea de tipo administrador
-
         $partido = new Partido();
         $partido->clubLocalPartido = $request->input('clubLocalPartido');
         $partido->clubVisitaPartido = $request->input('clubVisitaPartido');
@@ -134,16 +106,11 @@ class PartidoController extends Controller
         $partido->idArbitroAsistente1 = $request->input('idArbitroAsistente1');
         $partido->idArbitroAsistente2 = $request->input('idArbitroAsistente2');
         $partido->idCuartoArbitro = $request->input('idCuartoArbitro');
-
-
         
         $partido->save();
-
         return Redirect::to('partido');
     }
 //-------------------------------------------------------------------------------------------------------------------
-
-
     /**
      * Display the specified resource.
      *
@@ -152,7 +119,6 @@ class PartidoController extends Controller
      */
     public function show($id)
     {   
-
         $partidos = Partido::findOrFail($id);
        
         $asociaciones=Asociacion::all();
@@ -164,11 +130,9 @@ class PartidoController extends Controller
         $arbitros=Arbitro::all();
         $todospartidos=Partido::all();
         $historial=Historial::all();
-
         $mes = date("m");
         $dia = date("d");
         //dd($mes);
-
         $partidos_historial = array(array('local','visita','goles_local','goles_visita'));
         $contador = 0;
         foreach ($todospartidos as $todos) {
@@ -185,7 +149,6 @@ class PartidoController extends Controller
         $jugadores = Jugador::all();
         $trayectoriasjugadores = TrayectoriaJugador::all();
         $historiales = DB::table('Historiales')->get();
-
        // dd($partidos);
         //$jugadorHistorial = $historiales['idJugador'];
         $gol_jugador = array(array('jugador','gol','minutos_jugados'));
@@ -206,40 +169,27 @@ class PartidoController extends Controller
                     ->join('Partidos', 'Partidos.clubLocalPartido','=','Jugadores.idClub')
                     ->get();
                // dd($jugadorclublocal);
-
         $jugadorclubvisita = DB::table('Jugadores')
                     ->join('Partidos', 'Partidos.clubVisitaPartido','=','Jugadores.idClub')
                     ->get();
        
         $jugador_partido =DB::table('Jugadores')
                         ->join('TrayectoriasJugadores', 'TrayectoriasJugadores.idJugador', '=','Jugadores.idJugador')
-
-
                         ->get();
-
-
         $plantilla = DB::table('Historiales')
                     ->join('Jugadores', 'Historiales.idJugador','=','Jugadores.idJugador')
                     ->join('Partidos', 'Historiales.idPartido', '=', 'Partidos.idPartido')
                     ->join('TrayectoriasJugadores', 'Jugadores.idJugador', '=', 'TrayectoriasJugadores.idJugador')
                     ->get();
                     //sdd($plantilla);
-
-
                      #   dd($jugador_partido);
-
-
-
 ;
-
          return view('partido.show',['contador2'=>$contador2,'gol_jugador'=>$gol_jugador,'contador'=>$contador,'partidos_historial'=>$partidos_historial,'todospartidos' => $todospartidos, 'paises' => $paises,'arbitros' => $arbitros, 'partidos' => $partidos, 'clubes' => $clubes, 'torneos' => $torneos, 'id' => $id, 'estadios' => $estadios, 'jugadores' => $jugadores, 'trayectoriasjugadores' => $trayectoriasjugadores, 'historiales' => $historiales, 'jugador_partido' => $jugador_partido, 'jugadorclublocal' => $jugadorclublocal, 'jugadorclubvisita' => $jugadorclubvisita, 'plantilla' => $plantilla, 'mes' => $mes, 'dia' => $dia]);
                 
     }
-
 //---------------------FUNCION BUSCADOR----------------------------------------------------------------------------
     public function buscador($id)
     {   
-
         $partidos = Partido::findOrFail($id);
        
         $asociaciones=Asociacion::all();
@@ -253,9 +203,7 @@ class PartidoController extends Controller
         return view('partido.indexbuscador',  ['partidos' => $partidos, 'clubes' => $clubes, 'torneos' => $torneos, 'fecha' =>$fecha]);
                 
     }
-
 //---------------------------------Funcion que retorna las variables para el edit--------------------------------    
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -265,7 +213,6 @@ class PartidoController extends Controller
     public function edit(Request $request, $id)
     {
         $request->user()->authorizeRoles('admin'); //Se valida que el usuario que verá estos datos sea de tipo administrador
-
         $partidos = Partido::findOrFail($id);
         $asociaciones=Asociacion::all();
         $clubes=Club::all();
@@ -278,9 +225,6 @@ class PartidoController extends Controller
         return view('partido.edit', ['partidos' => $partidos,'asociaciones' => $asociaciones, 'ciudades' => $ciudades, 'clubes' => $clubes, 'estadios' => $estadios, 'paises' => $paises, 'torneos' => $torneos, 'arbitros' => $arbitros]);
     }
 //-------------------------------------------------------------------------------------------------------------------
-
-
-
 //---------------------Funcion actualizar un dato-------------------------------------------------------------------
     /**
      * Update the specified resource in storage.
@@ -292,7 +236,6 @@ class PartidoController extends Controller
     public function update(Request $request, $id)
     {   
         $request->user()->authorizeRoles('admin'); //Se valida que el usuario que verá estos datos sea de tipo administrador
-
         $partido = Partido::findOrFail($id);
         
         $partido->clubLocalPartido = $request->input('clubLocalPartido');
@@ -312,14 +255,10 @@ class PartidoController extends Controller
         $partido->idCuartoArbitro = $request->input('idCuartoArbitro');
   
         $partido->update();
-
         return Redirect::to('partido');
     }
 //-------------------------------------------------------------------------------------------------------------------
-
-
 //-------------------Funcion para eliminar (borrado fisico)------------------------------------------------------- 
-
     /**
      * Remove the specified resource from storage.
      *
@@ -329,22 +268,15 @@ class PartidoController extends Controller
     public function destroy(Request $request, $id)
     {   
         $request->user()->authorizeRoles('admin'); //Se valida que el usuario que verá estos datos sea de tipo administrador
-
         $partidos = Partido::find($id);
         $partidos->delete();
-
         return Redirect::to('partido');
     }
-
-
 //------------FUNCION PARA AGREGAR JUGADORES A PLANTILLA DE PARTIDO.INDEX-----------
     
-
-
     public function plantillacreate(Request $request)
     {   
         $request->user()->authorizeRoles('user'); //Se valida que el usuario que verá estos datos sea de tipo user
-
         $asociaciones=Asociacion::all();
         $clubes=Club::all();
         $ciudades=Ciudad::all();
@@ -352,22 +284,14 @@ class PartidoController extends Controller
         $paises=Pais::all();
         $torneos=Torneo::all();
         $arbitros=Arbitro::all();
-
         return view('partido.plantillacreate', ['asociaciones' => $asociaciones->toArray(), 'ciudades' => $ciudades->toArray(), 'clubes' => $clubes->toArray(), 'estadios' => $estadios->toArray(), 'paises' => $paises->toArray(), 'torneos' => $torneos->toArray(), 'arbitros' => $arbitros->toArray()]);
     }
-
-
     public function editplantilla (Request $request, $id){
         $request->user()->authorizeRoles('admin');
     }
-
-
-
-
     public function updateplantilla(Request $request, $id)
     {   
         $request->user()->authorizeRoles('admin'); //Se valida que el usuario que verá estos datos sea de tipo administrador
-
         $partido = Partido::findOrFail($id);
         
         $partido->clubLocalPartido = $request->input('clubLocalPartido');
@@ -387,7 +311,6 @@ class PartidoController extends Controller
         $partido->idCuartoArbitro = $request->input('idCuartoArbitro');
   
         $partido->update();
-
         return Redirect::to('partido');
     }
 }
