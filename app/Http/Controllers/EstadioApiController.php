55ba3b67fc000612d2b7852cdac8c89a0532f8c6
MatiasKnasta <matias@knasta.com>
2020-03-26 22:06:21 -0300

    estadiocontroller

--- app/Http/Controllers/EstadioApiController.php
+++ app/Http/Controllers/EstadioApiController.php
@@ -24,4 +24,38 @@ class EstadioApiController extends Controller
         return response()->json($estadios,200);
 
     }
+    public function show($id)
+    {
+        $estadio = Estadio::findOrFail($id);
+        return response()->json($estadio,200);
+    }
+    public function store(Request $request)
+    {
+        $estadio = new Estadio();
+        $estadio->nombreEstadio = $request->input('name');
+        $estadio->inauguracionEstadio = $request->input('fecha');
+        $estadio->idPais = $request->input('pais');   
+        $estadio->idCiudad = $request->input('ciudad');     
+        $estadio->capacidadEstadio = $request->input('capacidad');
+        
+        $estadio->save();
+    }
+    public function update(Request $request, $id)
+    {
+        $estadio = Estadio::findOrFail($id);
+        
+        $estadio->nombreEstadio = $request->input('name');
+        $estadio->capacidadEstadio = $request->input('capacidad');
+        $estadio->idPais = $request->input('pais');   
+        $estadio->idCiudad = $request->input('ciudad');     
+        $estadio->inauguracionEstadio = $request->input('fecha');
+               
+  
+        $estadio->update();
+    }
+    public function destroy($id)
+    {
+        $estadios = Estadio::find($id);
+        $estadios->delete();
+    }
 }
